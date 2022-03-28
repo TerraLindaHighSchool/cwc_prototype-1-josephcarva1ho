8b4598adf26ccd32f2caf550326fea56b6622379
josephcarva1ho <89489503+josephcarva1ho@users.noreply.github.com>
2022-03-28 15:32:08 -0700

    1.4 completed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 5 - 7);
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed= 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +15,13 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed;
-    public float turnSpeed;
     void Update()
     {
-        //Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
